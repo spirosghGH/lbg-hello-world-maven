f5e316ed32840225ae0d5ea43ccd64240b896522
Stefan <spirosgh1995@gmail.com>
2025-06-10 12:51:12 +0100

    Added Stefan

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
+        System.out.println(sayHelloToSomeone("Stefan"));
     }
 
     public static String sayHello(){
